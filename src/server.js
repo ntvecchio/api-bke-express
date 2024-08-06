acecef1e18ceea38951be62c3fa0167870925090
Osvaldo Vecchio <osvaldovecchioneto28@gmail.com>
2024-08-06 15:51:23 -0300

    add lib express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
-console.log('Olá Mundo!')
+//const express = require('express')
+//import e from 'express'
+import express from 'express'
+
+const app = express()
+
+app.get('/',(req, res) => {
+    res.json(
+        {
+            message: "olá Mundo Express!",
+            success: "Sucesso ao executar API"
+        }
+    )
+})
+
+app.listen(3000,() => {
+    console.log('servidor rodando em http://localhost:3000')
+})
